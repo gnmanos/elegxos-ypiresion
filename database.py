9be6f1bc10e31f91584d0739f848c1ae762f6199
gnmanos <gnmanos@gmail.com>
2026-07-13 22:46:13 +0300

    Fix database path

--- database.py
+++ database.py
@@ -3,26 +3,54 @@ import sqlite3
 conn = sqlite3.connect("elegxos_ypiresion.db")
 cur = conn.cursor()
 
-fields = [
-    ("katigoria","TEXT"),
-    ("dn","TEXT"),
-    ("arithmos_oplou","TEXT"),
-    ("thesi_oplovastou","TEXT"),
-    ("paron_apon","TEXT"),
-    ("dria","TEXT"),
-    ("omada","TEXT"),
-    ("paratiriseis","TEXT")
-]
+cur.execute("""
+CREATE TABLE IF NOT EXISTS prosopiko (
 
-for name,typ in fields:
-    try:
-        cur.execute(
-            f"ALTER TABLE prosopiko ADD COLUMN {name} {typ}"
-        )
-    except:
-        pass
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+
+    vathmos TEXT,
+    onomateponymo TEXT,
+    asm TEXT,
+    tilefono TEXT,
+    tilefono_syggeni TEXT,
+    email TEXT,
+
+    typos_oximatos TEXT,
+    arithmos_kykloforias TEXT,
+
+    katigoria TEXT,
+    dn TEXT,
+    arithmos_oplou TEXT,
+    thesi_oplovastou TEXT,
+    paron_apon TEXT,
+    dria TEXT,
+    omada TEXT,
+    paratiriseis TEXT
+)
+""")
+
+cur.execute("""
+CREATE TABLE IF NOT EXISTS users (
+    username TEXT,
+    password TEXT,
+    role TEXT
+)
+""")
+
+cur.execute("""
+SELECT COUNT(*)
+FROM users
+""")
+
+if cur.fetchone()[0] == 0:
+
+    cur.execute("""
+    INSERT INTO users
+    VALUES
+    ('admin','1234','ADMIN')
+    """)
 
 conn.commit()
 conn.close()
 
-print("Η βάση ενημερώθηκε")
+print("Η βάση δημιουργήθηκε")
